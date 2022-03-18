d8856febdb66f7b8a1ee300023fe951b24329df6
ALKR <61777514+ALKR-HACKHECKZ@users.noreply.github.com>
2022-03-18 11:28:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ALKR-HACKHECKZ/princy-.git /EvaMaria
+  git clone https://github.com/ALKR-HACKHECKZ/princybot.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
